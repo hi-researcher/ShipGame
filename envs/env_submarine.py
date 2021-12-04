5f7f60b3198974f9ae0e082c425172ff0a634d22
hi-researcher <liwei.1996@aliyun.com>
2021-12-04 17:15:57 +0800

    update submarine env

---
+++ envs/env_submarine.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# author:Nolan
+# datetime:2021/12/4 17:10
+# software: PyCharm
+class EnvSubMarine:
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reward(self):
+        pass
